3849ed7d1048d67cd6cd25ac3ebba6aae8a80236
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-04-21 15:52:28 +0530

    added the number guesser code

---
+++ Number_guesser/numberGuessing.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int min=0,max =100,usr;
+	int flag = 1;
+	srand(time(0));
+	int random_number = rand() % (max - min+1)+min;
+	
+	
+	while(flag){
+		printf("Guess the number: ");
+		scanf("%d",&usr);
+		
+		if(usr==random_number){
+				printf("YAY you got it right the number was inteed %d \n",random_number);
+				flag = 0;
+			}
+		if(usr>random_number){
+				printf("Try again \t Hint: try Lower\n");
+			}
+		if(usr<random_number){
+				printf("Try again \t Hint: try Higher\n");
+			}
+
+		}
+		return(0);
+}
+
+
