e8caaee018fecab66a6bac89b2f4393974d14218
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-04-21 15:53:24 +0530

    added the quadratic code

---
+++ Perfect_Number/perfectNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+    scanf("%d", &num);
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    if (sum == num && num != 0) {
+        printf("Perfect number\n");
+    } else {
+        printf("Not a perfect number\n");
+    }
+    return 0;
+}
+
