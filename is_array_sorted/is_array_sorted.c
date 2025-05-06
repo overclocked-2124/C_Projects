1c89d489d0f74438ebabf55ef4ff5e94ae38ee46
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-05-06 17:26:21 +0530

    added is_array_sorted.c

---
+++ is_array_sorted/is_array_sorted.c
@@ -0,0 +1,29 @@
+//
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sorted = 1;  
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            sorted = 0;
+            break;
+        }
+    }
+
+    if (sorted) {
+        printf("The array is sorted in accending order.\n");
+    } else {
+        printf("The array is not sorted.\n");
+    }
+    return 0;
+}
